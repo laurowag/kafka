dd2ade77fde9940575a3585f3a8052352f99b4cc
Lauro Wagnitz <lauro@agrotis.com>
2018-07-25 21:53:33 -0300

    teste kafka

---
+++ src/main/java/br/com/teste/Consumer.java
@@ -0,0 +1,52 @@
+package br.com.teste;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Properties;
+
+import org.apache.avro.Schema;
+import org.apache.avro.io.DecoderFactory;
+import org.apache.avro.reflect.ReflectData;
+import org.apache.avro.reflect.ReflectDatumReader;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+public class Consumer {
+
+	public static void main(String[] args) throws IOException {
+		
+		Properties props = new Properties();
+		props.put("bootstrap.servers", "localhost:9092");
+		props.put("group.id","test");
+		props.put("enable.auto.commit","true");
+		props.put("key.deserializer","org.apache.kafka.common.serialization.StringDeserializer");
+		props.put("value.deserializer","org.apache.kafka.common.serialization.ByteArrayDeserializer");
+		props.put("max.partition.fetch.bytes","2097152");		
+		
+		KafkaConsumer<String, byte[]> consumer = new KafkaConsumer<String, byte[]>(props);
+		
+		Schema s = ReflectData.get().getSchema(Cliente.class);
+		ReflectDatumReader<Object> reader = new ReflectDatumReader<Object>(s);
+
+        consumer.subscribe(Arrays.asList("test"));
+        int timeouts = 0;
+
+        while (true) {
+            ConsumerRecords<String, byte[]> records = consumer.poll(200);
+            if (records.count() == 0) {
+                timeouts++;
+            } else {
+            	System.out.printf("Got %d records after %d timeouts\n", records.count(), timeouts);
+            }
+
+            for (ConsumerRecord<String, byte[]> record: records) {
+            	Cliente cliente = (Cliente) reader.read(null,
+            		                DecoderFactory.get().binaryDecoder(record.value(), null));
+            	System.out.println(cliente.getNome());
+            }
+        }
+        
+	}
+
+}
---
+++ src/main/java/br/com/teste/Producer.java
@@ -0,0 +1,47 @@
+package br.com.teste;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.avro.Schema;
+import org.apache.avro.io.EncoderFactory;
+import org.apache.avro.reflect.ReflectData;
+import org.apache.avro.reflect.ReflectDatumWriter;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+public class Producer {
+
+	public static void main(String[] args) throws IOException {
+		
+		Properties props = new Properties();
+		props.put("bootstrap.servers", "localhost:9092");
+		props.put("group.id","test");
+		props.put("enable.auto.commit","true");
+		props.put("key.serializer","org.apache.kafka.common.serialization.StringSerializer");
+		props.put("value.serializer","org.apache.kafka.common.serialization.ByteArraySerializer");
+		props.put("max.partition.fetch.bytes","2097152");
+		
+		KafkaProducer<String, Object> producer = new KafkaProducer<>(props);
+		
+		
+		Schema s = ReflectData.get().getSchema(Cliente.class);
+		ReflectDatumWriter<Object> writer = new ReflectDatumWriter<Object>(s);
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		
+		try {
+			for (int i = 0; i < 1000; i++) {
+				Cliente cliente = new Cliente();
+				cliente.setCodigo(Long.parseLong(String.valueOf(i)));
+				cliente.setNome("Cliente "+i);
+				writer.write(cliente, EncoderFactory.get().directBinaryEncoder(out, null));
+				producer.send(new ProducerRecord<String, Object>("test", "chave"+i, out.toByteArray()));
+			}			
+		} finally {
+			producer.close();
+		}
+
+	}
+
+}
